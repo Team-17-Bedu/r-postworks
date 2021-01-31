ffc39ef0d6bf2ca9518781a44cec9e6ec01e4ffd
Iván Montiel cardona <ivan_montiel2013@hotmail.com>
2021-01-30 18:38:14 -0600

    Corrección de comentarios

--- src/Sesion-01/Postwork-01.R
+++ src/Sesion-01/Postwork-01.R
@@ -1,57 +1,61 @@
-#postowork sesion 1
+# Postwork Sesion 1
+
 library(dplyr)
 library(janitor)
 install.packages("tidyr")
 library(tidyr)
 
 
-setwd("C:/Users/abraham/Documents/DATASCIENCE/2daetapa/postwork/r-postworks/src/1erpostworked/dataset")
+# Asignaci√≥n dinamica de espacio de trabajo
+dir <- paste0(getwd(), "/src/Sesion-01/datasets")
+setwd(dir)
 
 Futbol <-"https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
 
 download.file(url = Futbol, destfile = "SP1.csv", mode = "wb")
 
 data <- read.csv("SP1.csv")
-#dejamos la columna de goles visitantes y de locales 
+# Dejamos la columna de goles visitantes y de locales
 datagoles<-data[,c('FTHG','FTAG')]
 
-#obtener el total de datos 
+# Obtener el total de datos
 total_datos<-length(datagoles$FTHG)
 
-#Tabla de frecuencia de los locales 
+# Tabla de frecuencia de los locales
 TF_locales<-datagoles%>%group_by(FTHG)%>%summarise(frequency=n())
 
-#Tabla de frecuencia de los visitanters 
+# Tabla de frecuencia de los visitanters
 TF_visitantes<-datagoles%>%group_by(FTAG)%>%summarise(frequency=n())
 
-#probabilidad de los locales marginal 
+# Probabilidad de los locales marginal
 probabilidad_marginal_local<-TF_locales$frequency /total_datos
 
-#probabiliad de los visitantes marginal 
+# Probabiliad de los visitantes marginal
 probabilidad_marginal_visitantes<-TF_visitantes$frequency/total_datos
 
-#creamos la tabla de los locales con su frecuencia y probabilidad 
+# Creamos la tabla de los locales con su frecuencia y probabilidad
 TablaLocales<-data.frame(TF_locales,probabilidad_marginal_local)
 TablaLocales$NGoles=TablaLocales$FTHG
-#creamos la tabla de los visitantes con su frecuencia y probabilidad 
+
+# Creamos la tabla de los visitantes con su frecuencia y probabilidad
 TablaVisitantes<-data.frame(TF_visitantes,probabilidad_marginal_visitantes)
 TablaVisitantes$NGoles=TablaVisitantes$FTAG
 
-#se agrega nueva linea para que la tabla de visitantes tenga los mismos tamaÒos y no exista problema al poder hacer alguna operacion
+# Se agrega nueva linea para que la tabla de visitantes tenga los mismos tamaÔøΩos y no exista problema al poder hacer alguna operacion
 nuevalinea=data.frame(FTAG=c(0),frequency=c(0),probabilidad_marginal_visitantes=c(0),NGoles=c(6))
 TablaVisitantes=rbind(TablaVisitantes,nuevalinea)
 
-#unimos las dos tablas creadas anteriormente para poder sacar la probabilidad conjunta 
-#newtable<-merge(TablaLocales,TablaVisitantes,by="NGoles",all.x = TRUE,)
-#newtable$sum_probabilidades=TablaLocales$probabilidad_marginal_local+TablaVisitantes$probabilidad_marginal_visitantes
-#newtable$sum_frecuencia=TablaLocales$frequency+TablaVisitantes$frequency
+# Unimos las dos tablas creadas anteriormente para poder sacar la probabilidad conjunta
+# newtable<-merge(TablaLocales,TablaVisitantes,by="NGoles",all.x = TRUE,)
+# newtable$sum_probabilidades=TablaLocales$probabilidad_marginal_local+TablaVisitantes$probabilidad_marginal_visitantes
+# newtable$sum_frecuencia=TablaLocales$frequency+TablaVisitantes$frequency
 
 
-#prueba ver que sucede 
+# Prueba ver que sucede
 tabla_conjunta <- unite(datagoles,Goles,c(1:2),  sep = " ", remove = TRUE)
 tablaConjunta_frecuencia <-tabla_conjunta %>%group_by(Goles) %>% summarise(frequency = n())
 
-#sacar probabilidad conjunta 
+# Sacar probabilidad conjunta
 tablacojunta_probabilidad <- tablaConjunta_frecuencia$frequency / total_datos
 
 TablaFinal <- data.frame(tablaConjunta_frecuencia,tablacojunta_probabilidad)
--- src/Sesion-02/Postwork-02.R
+++ src/Sesion-02/Postwork-02.R
@@ -1,62 +1,68 @@
-#postwork sesion 2 
+# Postwork sesion 2
 
-#se llama a la libreria dplyr
+# Se llama a la libreria dplyr
 library(dplyr)
 
-#fijamos directorio 
-setwd("C:/Users/abraham/Documents/DATASCIENCE/2daetapa/postwork/r-postworks/src/2dopostwork/datasets")
+# Asignaci√≥n dinamica de espacio de trabajo
+dir <- paste0(getwd(), "/src/Sesion-02/datasets")
+setwd(dir)
 
-#guardamos los links en una variable 
+# Guardamos los links en una variable
 dataurl1819 <- "https://www.football-data.co.uk/mmz4281/1819/SP1.csv"
 dataurl1920 <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
 dataurl1718 <- "https://www.football-data.co.uk/mmz4281/1718/SP1.csv"
 
-#descargamos los datasets
+# Descargamos los datasets
 download.file(dataurl1718, destfile = "dataurl1718.csv", mode = "wb")
 download.file(dataurl1819, destfile = "dataurl1819.csv", mode = "wb")
 download.file(dataurl1920, destfile = "dataurl1920.csv", mode = "wb")
 
-#leer los archivos descargados con el comando lapply
+# Leer los archivos descargados con el comando lapply
 lista <- lapply(list.files(pattern ="*.csv"), read.csv)
 
 
-#2.Obten una mejor idea de las caracterÌsticas de los data frames al usar las funciones: str, head, View y summary
+# 2. Obten una mejor idea de las caracteristicas de los data frames al usar las funciones: str, head, View y summary
 
-#realizamos el segundo punto con el primer dataset de la lista 
+# Realizamos el segundo punto con el primer dataset de la lista
 str(lista[[1]]); head(lista[[1]]); summary(lista[[1]]); View(lista[[1]])
 
-#realizamos el segundo punto con el segundo dataset de la lista 
+# Realizamos el segundo punto con el segundo dataset de la lista
 str(lista[[1]]); head(lista[[2]]); summary(lista[[2]]); View(lista[[2]])
 
-#realizamos el segundo punto con el tercer dataset de la lista 
+# Realizamos el segundo punto con el tercer dataset de la lista
 str(lista[[3]]); head(lista[[3]]); summary(lista[[3]]); View(lista[[3]])
 
-#3 Con la funciÛn select del paquete dplyr selecciona ˙nicamente las columnas Date, HomeTeam, AwayTeam, FTHG, FTAG y FTR; esto para cada uno de los data frames. (Hint: tambiÈn puedes usar lapply).
+# 3. Con la funcion select del paquete dplyr selecciona ÔøΩnicamente las columnas Date,
+# HomeTeam, AwayTeam, FTHG, FTAG y FTR; esto para cada uno de los data frames.
+# (Hint: tambien puedes usar lapply).
 
-#ejecutamos el comando para poder seleccionar las columnas requeridas el punto 3 
+# Ejecutamos el comando para poder seleccionar las columnas requeridas el punto 3
 lista <- lapply(lista,select, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
 
-#vemos como se ve el primer dataframe
+# Vemos como se ve el primer dataframe
 head(lista[1])
 
-#vemos como se ve el segundo dataframe
+# Vemos como se ve el segundo dataframe
 head(lista[2])
 
-#vemos como se ve el tercer dataframe
+# Vemos como se ve el tercer dataframe
 head(lista[3])
 
-#4 Aseg˙rate de que los elementos de las columnas correspondientes de los nuevos data frames sean del mismo tipo (Hint 1: usa as.Date y mutate para arreglar las fechas). Con ayuda de la funciÛn rbind forma un ˙nico data frame que contenga las seis columnas mencionadas en el punto 3 (Hint 2: la funciÛn do.call podrÌa ser utilizada).
+# 4. Asegurate de que los elementos de las columnas correspondientes de los nuevos
+# data frames sean del mismo tipo (Hint 1: usa as.Date y mutate para arreglar las fechas).
+# Con ayuda de la funcion rbind forma un unico data frame que contenga las seis columnas
+# mencionadas en el punto 3 (Hint 2: la funcion do.call podria ser utilizada).
 
-#se combina los datasets 
+# Se combina los datasets
 data <- do.call(rbind, lista)
 
-#Se verifica que todos los datos sean igual 
+# Se verifica que todos los datos sean igual
 str(data)
 
-#arreglamos las fechas con mutate
+# Arreglamos las fechas con mutate
 data <- mutate(data, Date = as.Date(Date, "%d/%m/%y"))
 
-#vemos como quedaron las fechas 
+# Vemos como quedaron las fechas
 head(data)
 
 
